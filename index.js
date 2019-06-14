70bb3f4b3b484298d5850e3fd44422394109a66e
Simon Maher <simon.maher@integrify.io>
2019-06-14 10:41:22 +0300

    Created consts for hours, minutes and seconds

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const clock = document.querySelector('.clock')
 
 const tick = () => {
     const now = new Date()
+    const hours = now.getHours()
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
+
 }
 
 setInterval(tick, 1000)
